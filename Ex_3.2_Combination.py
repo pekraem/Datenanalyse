a256a7258a901ac0c623c9c75a6820f3252a92ff
makoeppel <koeppel.ma@googlemail.com>
2016-05-18 18:17:15 +0200

    Ex_3.2 endlich fertig und richtig :D

--- Ex_3.2_Combination.py
+++ Ex_3.2_Combination.py
@@ -1,70 +1,36 @@
+#!/usr/bin/env python
+from ROOT import TMinuit,Double,Long
 import numpy as np
-from ROOT import *
-
-##______________________________________________________________________________
-def testfit():
-
-   gMinuit = TMinuit(1)
-   gMinuit.SetFCN( fcn )
-
-   arglist = array( 'd', 10*[0.] )
-   ierflg = 1982
-
-   arglist[0] = 1
-   gMinuit.mnexcm( "SET ERR", arglist, 1, ierflg )
-
- # Set starting values and step sizes for parameters
-   vstart = array( 'd', ( 3,  1,  0.1,  0.01  ) )
-   step   = array( 'd', ( 0.1, 0.1, 0.01, 0.001 ) )
-   gMinuit.mnparm( 0, "a1", vstart[0], step[0], 0, 0, ierflg )
-   gMinuit.mnparm( 1, "a2", vstart[1], step[1], 0, 0, ierflg )
-   gMinuit.mnparm( 2, "a3", vstart[2], step[2], 0, 0, ierflg )
-   gMinuit.mnparm( 3, "a4", vstart[3], step[3], 0, 0, ierflg )
-
- # Now ready for minimization step
-   arglist[0] = 500
-   arglist[1] = 1.
-   gMinuit.mnexcm( "MIGRAD", arglist, 2, ierflg )
-
- # Print results
-   amin, edm, errdef = 0.18, 0.19, 0.20
-   nvpar, nparx, icstat = 1983, 1984, 1985
-   gMinuit.mnstat( amin, edm, errdef, nvpar, nparx, icstat )
-   gMinuit.mnprin( 3, amin )
-
-
-##______________________________________________________________________________
-def fcn( npar, gin, f, par, iflag ):
-   global ncount
-   nbins = 1
-
- # calculate chisquare
-   chisq, delta = 0., 0.
-   for i in range(nbins):
-      delta  = (m_w[i]-par)/errorw[i]
-      chisq += delta*delta
-
-   f[0] = chisq
-   ncount += 1
-
- 
- 
-
-m_w = [80457, 80448]
-
-
-error_w1 = [30,11,47,17,17]
-error_w2 = [33,12,0,19,17]
+from array import array as arr
+import matplotlib.pyplot as plt
 
 
 
-errorw = [
+# --> this is the definition of the function to minimize, here a chi^2-function
+def calcChi2(npar, mean):
+    delta = np.array([[Double(m_w[0]) - mean[1]], [Double(m_w[1]) - mean[1]]])
+    delta_T = delta.T
+    chi2 = np.dot(delta_T , np.dot(kor_matirx, delta))
+    return chi2
 
-
+def fcn(npar, deriv, f, apar, iflag):
+    """ meaning of parametrs:
+         npar:   number of parameters
+         deriv:  aray of derivatives df/dp_i (x), optional
+         f:      value of function to be minimised (typically chi2 or negLogL) 
+         apar:   the array of parameters
+         iflag:  internal flag: 1 at first call, 3 at the last, 4 during minimisation
+    """
+    f[0] = calcChi2(npar,apar) 
+    
+global m_w
+m_w = [80457., 80448.]
+error_w1 = [30.,11.,47.,17.,17.]
+error_w2 = [33.,12.,0.,19.,17.]
+npar = 1 #maximum of npar parameters
 
 g1 = 0.
 g2 = 0.
-
 for i in range(len(error_w1)):
   g1 += error_w1[i]**2
   g2 += error_w2[i]**2
@@ -72,17 +38,41 @@ for i in range(len(error_w1)):
 s = error_w1[3]*error_w2[3]
 t = error_w1[4]*error_w2[4]
 
+global kor_matirx
 kor_matirx = np.array([[g1, s+t], [g2, s+t]])
 
-mean = (m_w1 + m_w2)/2.
 
-delta = np.array([[m_w1 - mean], [m_w2 - mean]])
-delta_T = delta.T
-chi2 = delta_T * kor_matirx * delta
+# --> set up MINUIT
+myMinuit = TMinuit(npar)  # initialize TMinuit with maximum of npar parameters
+myMinuit.SetFCN(fcn)      # set function to minimize
+arglist = arr('d', 2*[0.01]) # set error definition 
+ierflg = Long(0)             
+arglist[0] = 1.              # 1 sigma is Delta chi^2 = 1
+myMinuit.mnexcm("SET ERR", arglist ,1,ierflg)
 
 
-print delta
-print kor_matirx
-print delta_T
+# --> Set starting values and step sizes for parameters
+# Define the parameters for the fit
+myMinuit.mnparm(1, "mean", 400, 0.001, 0,0,ierflg)
 
-print chi2
+arglist[0] = 6000 # Number of calls to FCN before giving up.
+arglist[1] = 0.3  # Tolerance
+myMinuit.mnexcm("MIGRAD", arglist ,2,ierflg)  # execute the minimisation
+
+# --> check TMinuit status 
+amin, edm, errdef = Double(0.), Double(0.), Double(0.)
+nvpar, nparx, icstat = Long(0), Long(0), Long(0)
+myMinuit.mnstat(amin,edm,errdef,nvpar,nparx,icstat)
+# meaning of parameters:
+#   amin: value of fcn at minimum (=chi^2)
+#   edm:  estimated distance to mimimum
+#   errdef: delta_fcn used to define 1 sigma errors
+#   nvpar: number of variable parameters
+#   nparx: total number of parameters
+#   icstat: status of error matrix: 
+#           3=accurate 
+#           2=forced pos. def 
+#           1= approximative 
+#           0=not calculated
+myMinuit.mnprin(3,amin) # print-out by Minuit
+
