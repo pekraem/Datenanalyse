b2d193fa407ed3e77c5c8ca940babd671c01daf5
JoergPhysics <Joerg-Schindler@gmx.de>
2016-06-02 10:27:40 +0200

    Changed mean and chi2 into floats; output added

--- Blatt-5/Ex_5.1_new_chi2.py
+++ Blatt-5/Ex_5.1_new_chi2.py
@@ -1,30 +1,23 @@
-import random as rndm
-
+#Claculate Chi2
 def chi2(a,b):
-  chi=0
+  chi=0.
   for i in a:
-    chi += (i-b)**2/b
+    chi += ((i-b)**2)/b
   return chi
 
-
+#Input Data
 tracks = range(1,9)
 winners = [29, 19, 18, 25, 17, 10, 15, 11]
-#winners = []
 
+#print tracks, len(tracks)
+#print winners, len(winners)
 
-
-
-print tracks, len(tracks)
-print winners, len(winners)
-
-#for i in range(8):
-  #winners.append(rndm.gauss(18,10))
-
-mean = 0
+#Calculate the mean of the winners per track
+mean = 0.
 for n in winners:
   mean += n
 mean /= len(winners)
-
+#Significance
 sig = [14.07, 18.48]
   
 print mean
@@ -32,3 +25,13 @@ print mean
 print sig
 
 print chi2(winners,mean)
+
+if sig[1]>=chi2(winners,mean):
+  print "Significane level 1%: The hypothesis is true"
+else:
+  print "Significane level 1%: The hypothesis is not true"
+
+if sig[0]>=chi2(winners,mean):
+  print "Significane level 5%: The hypothesis is true"
+else:
+  print "Significane level 5%: The hypothesis is not true"
