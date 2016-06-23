858a3f0988c6dde1c6c7d9dda125794e0d277ccd
pekraem <upcnb@student.kit.edu>
2016-06-23 14:56:04 +0200

    addet GetX()

--- Blatt-8/Ex_9.2_likelihood.py
+++ Blatt-8/Ex_9.2_likelihood.py
@@ -26,6 +26,9 @@ log_likeli.Draw()
 
 c.SaveAs('log_likelihood.pdf')
 m=log_likeli.GetMinimum()
+xm=log_likeli.GetX(m,0,10)
+
+print m, xm
 
 c.Close()
 print 'minimum is at v_t = ', m
@@ -34,11 +37,22 @@ c1 = TCanvas('c1','c1',800,600)
 cl_likeli = TF1('cl_likeli',p_likeli_cl,0,30,2)
 eins = TLine(0,1,30,1)
 eins.SetLineColor(4)
+zwei = TLine(0,1.28,30,1.28)
+zwei.SetLineColor(3)
 cl_likeli.SetParameter(0,3)
 cl_likeli.SetParameter(1,m)
 cl_likeli.Draw()
 eins.Draw("SAME")
+zwei.Draw("SAME")
+
 #c1.SaveAs('delta_log_likelihood.pdf')
+minlimit_68 = cl_likeli.GetX(1.,0,5)
+maxlimit_68 = cl_likeli.GetX(1.,5,15)
+print minlimit_68, maxlimit_68
+
+minlimit_90 = cl_likeli.GetX(1.28,0,5)
+maxlimit_90 = cl_likeli.GetX(1.28,5,15)
+print minlimit_90, maxlimit_90
 
 raw_input()
 c1.Close()
